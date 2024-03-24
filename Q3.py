a654c02738596d60105bb4a653cdc651e5910fc1
Lukasnog18 <luksnogueira18@gmail.com>
2024-03-24 16:38:30 -0300

    Teste Completo.

---
+++ Q3.py
@@ -0,0 +1,9 @@
+a) 1, 3, 5, 7, 9 (aumento de 2)
+
+b) 2, 4, 8, 16, 32, 64, 128 (multiplicação por 2)
+
+c) 0, 1, 4, 9, 16, 25, 36, 49 (quadrado da soma)
+
+d) 4, 16, 36, 64, 100 (Progressão Aritmética)
+
+e) 1, 1, 2, 3, 5, 8, 13 (sequência de Fibonacci)
---
+++ Q4.py
@@ -0,0 +1,11 @@
+Primeira visita à sala das lâmpadas:
+
+Ligue o primeiro interruptor e aguarde alguns minutos.
+Em seguida, desligue o primeiro interruptor e ligue o segundo interruptor.
+Entre na sala das lâmpadas.
+Observação na primeira visita:
+
+Se a lâmpada estiver acesa, então o primeiro interruptor controla essa lâmpada.
+Se a lâmpada estiver apagada, toque na lâmpada para sentir a temperatura.
+Se estiver quente, então o segundo interruptor controla essa lâmpada.
+Se estiver fria, então o terceiro interruptor controla essa lâmpada.
